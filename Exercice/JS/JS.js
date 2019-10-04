cbf924b9796e8c55e2287dff58e03e9f7f9b6266
davidroulet <47849716+davidroulet@users.noreply.github.com>
2019-10-04 15:03:51 +0200

    Exo

--- Exercice/JS/JS.js
+++ Exercice/JS/JS.js
@@ -5,8 +5,10 @@ function init() {
     D3.addEventListener("mouseover",Div3)
     D3.addEventListener("mouseout",Div32)
     D4.addEventListener("click",Div4)
+    D5.addEventListener("click",Div5)
 
 }
+var YAS="YAS"
 function Div1(){
      D1.style.backgroundColor="lightblue"
      D1.style.textDecoration="line-through"
@@ -24,6 +26,12 @@ function Div32(){
 function Div4() {
     if(Cb1D4.checked&&Cb2D4.checked){
         D4.style.backgroundColor="Green"
-        D4.style.fontcolor()=white
+        D4.style.color="white"
+        document.getElementById("D4").innerHTML = "YAS"
+        D4.style.border="solid 0px black"
     }
+
+}
+function Div5() {
+    document.getElementById("D5").innerHTML = "Quand on clique sur ce texte, ça détruit"
 }
