ef44cd749d373bcd935da8f3f46943cc6c2555b9
davidroulet <47849716+davidroulet@users.noreply.github.com>
2019-10-07 14:38:50 +0200

    Exo 6

--- Exercice/JS/JS.js
+++ Exercice/JS/JS.js
@@ -6,9 +6,9 @@ function init() {
     D3.addEventListener("mouseout",Div32)
     D4.addEventListener("click",Div4)
     D5.addEventListener("click",Div5)
+    ED6.addEventListener("change",Div6)
 
 }
-var YAS="YAS"
 function Div1(){
      D1.style.backgroundColor="lightblue"
      D1.style.textDecoration="line-through"
@@ -34,4 +34,18 @@ function Div4() {
 }
 function Div5() {
     document.getElementById("D5").innerHTML = "Quand on clique sur ce texte, ça détruit"
+}
+function Div6(){
+    inisale=""
+    longeur=0
+    Space =""
+    inisale=ED6.value.substr(0,1)
+    console.log(inisale)
+    Space = ED6.value.lastIndexOf(" ")
+    inisale=inisale+ED6.value.substr(Space+1 ,1)
+    console.log(inisale)
+    longeur=ED6.value.length
+    inisale=inisale+ED6.value.substr(longeur-1,1)
+    console.log(inisale)
+    SD6.value=inisale.toUpperCase()
 }
