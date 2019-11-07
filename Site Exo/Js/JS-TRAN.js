c467d163437eac707cdc43239321d8ef4892af59
davidroulet <47849716+davidroulet@users.noreply.github.com>
2019-11-07 15:08:53 +0100

    Exo Site Tablo

--- Site Exo/Js/JS-TRAN.js
+++ Site Exo/Js/JS-TRAN.js
@@ -16,16 +16,58 @@ function MOD() {
     for (i=1;i<Nb+1;i++){
         var NomEl
         NomEl = "CHENR"+i
-        console.log(i)
-        console.log(NomEl)
         document.getElementById(NomEl).classList.remove("d-none")
     }
-
+TableADD.classList.remove("d-none")
 }
 function ADD() {
-    console.log("ADD")
-}
-var Nb=3;
-function REM() {
+    if(PLA.value.length==0||MAJ.value.length==0||NAME.value.length==0||PNAME.value.length==0){
+        alert("Il manque des Inforamtion!")
+    }else {
+        Nb++
+        inisale = ""
+        longeur = 0
+        Space = ""
+        inisale = PNAME.value.substr(0, 1)
+        console.log(inisale)
+        inisale = inisale + NAME.value.substr(0, 1)
+        console.log(inisale)
+        longeur = NAME.value.length
+        inisale = inisale + NAME.value.substr(longeur - 1, 1)
+        console.log(inisale)
+    newtr = document.createElement("tr")
+    newTd1 = document.createElement("td")
+    newTd2 = document.createElement("td")
+    newTd3 = document.createElement("td")
+    newTd4 = document.createElement("td")
+    newTd5 = document.createElement("td")
+    newTd6 = document.createElement("td")
+    newTd7 = document.createElement("td")
+        newInput= document.createElement("input")
+    newTd1.innerText = Nb
+    newTd2.innerText = inisale.toUpperCase()
+    newTd3.innerText = PLA.value
+    newTd4.innerText = " E"
+    newTd5.innerText = NAME.value
+    newTd6.innerText = PNAME.value
 
+newInput.setAttribute("type", "checkbox")
+        idnew="CHENR"+Nb
+        newInput.setAttribute("id", idnew);
+        newTd7.appendChild(newInput)
+        newtr.appendChild(newTd1)
+    newtr.appendChild(newTd2)
+    newtr.appendChild(newTd3)
+    newtr.appendChild(newTd4)
+    newtr.appendChild(newTd5)
+    newtr.appendChild(newTd6)
+        newtr.appendChild(newTd7)
+    Table.appendChild(newtr)
+}}
+var Nb=4;
+function REM() {
+    for (i=1;i<Nb+1;i++){
+        IDC="CHENR"+i
+        console.log(IDC)
+    }
 }
